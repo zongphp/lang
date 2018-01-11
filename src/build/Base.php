64c903642bf9b539588ac259230127dc95298fee
zmingzhi <zmingzhi@yeah.net>
2018-01-11 16:42:32 +0800

    2.0 version

--- src/build/Base.php
+++ src/build/Base.php
@@ -2,27 +2,39 @@
 namespace zongphp\lang\build;
 
 use zongphp\arr\Arr;
+use zongphp\config\Config;
 
-class Base {
-	//语句包
-	private $language;
+class Base
+{
+    //语句包
+    private $language;
 
-	/**
-	 * 语言包文件
-	 *
-	 * @param $file
-	 */
-	public function file( $file ) {
-		$this->language = include $file;
-	}
+    public function __construct()
+    {
+        $this->language = include(Config::get('lang.lang'));
+    }
 
-	//获取语言
-	public function get( $lang ) {
-		return Arr::get( $this->language, $lang );
-	}
+    /**
+     * 语言包文件
+     *
+     * @param $file
+     */
+    public function file($file)
+    {
+        $this->language = include $file;
+    }
 
-	//设置单个语言
-	public function set( $name, $value ) {
-		return Arr::set( $this->language, $name, $value );
-	}
+    //获取语言
+    public function get($lang)
+    {
+        return Arr::get($this->language, $lang);
+    }
+
+    //设置单个语言
+    public function set($name, $value)
+    {
+        $this->language = Arr::set($this->language, $name, $value);
+
+        return true;
+    }
 }
